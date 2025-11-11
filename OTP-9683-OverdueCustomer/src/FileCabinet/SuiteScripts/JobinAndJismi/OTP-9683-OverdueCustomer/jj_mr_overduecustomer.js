490f070b117721453e8b5038ad2eb2b217d4becd
Aishwarya3040 <aishwarya.sajan@jobinandjismi.com>
2025-11-11 11:56:01 +0530

    otp-9683-fix:add jsdoc tgs and import objects

--- OTP-9683-OverdueCustomer/src/FileCabinet/SuiteScripts/JobinAndJismi/OTP-9683-OverdueCustomer/jj_mr_overduecustomer.js
+++ OTP-9683-OverdueCustomer/src/FileCabinet/SuiteScripts/JobinAndJismi/OTP-9683-OverdueCustomer/jj_mr_overduecustomer.js
@@ -11,7 +11,7 @@
  *
  * Author: Jobin and Jismi IT Services
  *
- * Date Created : 11-November-2025
+ * Date Created : 1-November-2025
  *
  * Description : Map/Reduce script to identify overdue invoices, generate CSV summaries, and email customers.
  *
